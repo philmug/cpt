987f8ae19086f399bff512957073b6d638fc1fb5
philmug <83515066+philmug@users.noreply.github.com>
2021-05-03 22:53:53 -0400

    imported pygame, created the window and fps variables, initiated pygame, pygame mixer, created the window size and started on the game loop.

--- final project.py
+++ final project.py
@@ -1,1 +1,18 @@
+# This is importing both pygame and random
 import pygame
+import random
+
+Width= #tbd
+Height= #tbd
+FPS= #tbd
+
+#This is initiating pygame and the pygame mixer and it creates the window size.
+pygame.init()
+pygame.mixer.init()
+screen= pygame.display.set_mode((Width, Height))
+pygame.display.set_caption("name of game")
+clock= pygame.time.Clock()
+
+#Game loop
+Active= True
+while Active:
