212eb0bf8c46694851efd0f8aded7f545afd7f10
philmug <83515066+philmug@users.noreply.github.com>
2021-05-04 13:32:37 -0400

    created skeleton of game loop and created colour variables

--- final project.py
+++ final project.py
@@ -2,9 +2,16 @@
 import pygame
 import random
 
-Width= #tbd
-Height= #tbd
-FPS= #tbd
+Width= 500 #tbd
+Height= 500 #tbd
+FPS= 30 #tbd
+
+#this is defining some colours
+Black= (0, 0, 0)
+White= (255, 255, 255)
+Red= (255, 0, 0)
+Green= (0, 225, 0)
+Blue= (0, 0, 255)
 
 #This is initiating pygame and the pygame mixer and it creates the window size.
 pygame.init()
@@ -16,3 +23,17 @@ clock= pygame.time.Clock()
 #Game loop
 Active= True
 while Active:
+    #this makes sure the game loop is staying at a constant speed
+    clock.tick(FPS)
+    #this for loop is processing events
+    for event in pygame.event.get():
+        #this is statement checks if the player has quit
+        if event.type == pygame.QUIT:
+            #if they have it ends the game loop
+            Active= False
+
+    screen.fill(Black)
+
+    pygame.display.flip()
+
+pygame.quit()
