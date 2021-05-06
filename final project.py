043ee974f2721b8be957687b310dcec5f239e41c
philmug <83515066+philmug@users.noreply.github.com>
2021-05-05 22:10:38 -0400

    organized the game code and made a file called settings which hold main variables like window size and fps

--- final project.py
+++ final project.py
@@ -1,39 +1,63 @@
-# This is importing both pygame and random
+# This is importing both pygame and random.
 import pygame
 import random
+#This imports everything from the settings file
+from Settings import *
 
-Width= 500 #tbd
-Height= 500 #tbd
-FPS= 30 #tbd
+class Game:
+    def __init__(self):
+        #This is initiating pygame and the pygame mixer and it creates the window size.
+        pygame.init()
+        pygame.mixer.init()
+        self.screen= pygame.display.set_mode((Width, Height))
+        pygame.display.set_caption(Title)
+        self.clock= pygame.time.Clock()
+        self.Active = True
 
-#this is defining some colours
-Black= (0, 0, 0)
-White= (255, 255, 255)
-Red= (255, 0, 0)
-Green= (0, 225, 0)
-Blue= (0, 0, 255)
+    def new(self):
+        self.all_sprites = pygame.sprite.Group()
+        self.run()
 
-#This is initiating pygame and the pygame mixer and it creates the window size.
-pygame.init()
-pygame.mixer.init()
-screen= pygame.display.set_mode((Width, Height))
-pygame.display.set_caption("name of game")
-clock= pygame.time.Clock()
 
-#Game loop
-Active= True
-while Active:
-    #this makes sure the game loop is staying at a constant speed
-    clock.tick(FPS)
-    #this for loop is processing events
-    for event in pygame.event.get():
-        #this is statement checks if the player has quit
-        if event.type == pygame.QUIT:
-            #if they have it ends the game loop
-            Active= False
+    def run(self):
+        self.playing = True
+        while self.playing:
+            self.clock.tick(FPS)
+            self.events()
+            self.update()
+            self.draw()
 
-    screen.fill(Black)
 
-    pygame.display.flip()
+    def update(self):
+        self.all_sprites.update()
+
+    def events(self):
+        #this for loop is processing events
+        for event in pygame.event.get():
+            #this is statement checks if the player has quit
+            if event.type == pygame.QUIT:
+                if self.playing:
+                    self.playing = False
+                #if they have it ends the game loop
+                self.Active= False
+
+    def draw(self):
+        self.screen.fill(Black)
+        self.all_sprites.draw(self.screen)
+        pygame.display.flip()
+
+    def show_start_screen(self):
+        pass
+
+    def show_gameover_screen(self):
+        pass
+
+
+game= Game()
+game.show_start_screen()
+while game.Active:
+    game.new()
+    game.show_gameover_screen()
 
 pygame.quit()
+
