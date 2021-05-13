9ba944a0ef7fbfdf5eca10ecec145032879bc378
philmug <83515066+philmug@users.noreply.github.com>
2021-05-12 22:09:35 -0400

    sprites

---
+++ maps/Tileset1.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.6.0" name="Tileset" tilewidth="32" tileheight="32" spacing="1" tilecount="336" columns="24">
+ <image source="../img/sprites/Tileset.png" width="791" height="461"/>
+</tileset>
---
+++ maps/decore.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.6.0" name="[32x32] Decoraciones V1" tilewidth="32" tileheight="32" tilecount="16" columns="4">
+ <image source="../img/sprites/[32x32] Decoraciones V1.png" width="128" height="128"/>
+</tileset>
