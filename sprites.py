ea0a4efd27c92ab68d2c0f3c113db6b32f424a73
Josh <jowshgt@gmail.com>
2021-05-19 10:52:23 -0400

    fixed where arrow class/update was, now in sprites.py

--- sprites.py
+++ sprites.py
@@ -28,6 +28,37 @@ class Player(pygame.sprite.Sprite):
                             self.direction)
             bullet_group.add(bullet)
 
+class Arrow(pygame.sprite.Sprite):
+    def __init__(self, x, y, direction):
+        pygame.sprite.Sprite.__init__(self)
+        self.speed = 10
+        self.image = arrow.png
+        self.rect = self.image.get_rect()
+        self.rect.center = (x, y)
+        self.direction = direction
+
+    def update(self):
+        # move arrow
+        self.rect.x += (self.direction * self.speed) + screen_scroll
+        # check if arrow has gone off screen
+        if self.rect.right < 0 or self.rect.left > SCREEN_WIDTH:
+            self.kill()
+        # check for collision with level
+        for tile in world.obstacle_list:
+            if tile[1].colliderect(self.rect):
+                self.kill()
+
+        # check collision with characters
+        if pygame.sprite.spritecollide(player, arrow_group, False):
+            if player.alive:
+                player.health -= 5
+                self.kill()
+        for enemy in enemy_group:
+            if pygame.sprite.spritecollide(enemy, arrow_group, False):
+                if enemy.alive:
+                    enemy.health -= 25
+                    self.kill()
+
     def jump(self):
         # jump only if standing on a platform
         self.rect.x += 1
